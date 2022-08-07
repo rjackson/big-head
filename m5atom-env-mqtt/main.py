41d96aaa466cab0b5a6920ceb01db8c8b40f2818
Rob Jackson <rob@rjackson.me>
2022-08-07 17:17:54 +0100

    trying a bunch til wdt and interrupt handlers work

--- m5atom-env-mqtt/main.py
+++ m5atom-env-mqtt/main.py
@@ -6,34 +6,19 @@ import json
 import unit
 import network
 import ubinascii
+
 from machine import WDT
+from machine import Timer
 
 MQTT_SERVER='192.168.86.24'
 MQTT_PORT='1883'
 MQTT_TOPIC='m5atom-env-sensor'
+
 POLL_INTERVAL=10 # every 10 seconds  
-
-def blip():
-  rgb.setColorAll(0xff0000)
-  rgb.setBrightness(255)
-  wait_ms(100)
-  rgb.setBrightness(0)
+WATCHDOG_TIMEOUT=30000
+TICK_RESOLUTION=1000
   
-
-@timerSch.event('poll')
-def tpoll():
-  data = {
-    'temperature': env3_1.temperature,
-    'humidity': env3_1.humidity,
-    'pressure': env3_1.pressure,
-    'device_id': device_id
-  }
-  
-  m5mqtt.publish(MQTT_TOPIC, json.dumps(data))
-  blip();
-  wdt.feed()
-  
-wdt = WDT(timeout=2000)
+wdt = WDT(timeout=WATCHDOG_TIMEOUT)
 env3_1 = unit.get(unit.ENV3, unit.PORTA)
 wlan_sta = network.WLAN(network.STA_IF)
 wlan_sta.active(True)
@@ -44,4 +29,32 @@ print("Device ID:", device_id)
 
 m5mqtt = M5mqtt(device_id, MQTT_SERVER, MQTT_PORT)
 m5mqtt.start()
-timerSch.run('poll', POLL_INTERVAL * 1000, 0x00)
+
+tick = False
+def tock(event):
+  global tick
+  tick = True
+
+tim = Timer(0)
+tim.init(period=POLL_INTERVAL * 1000, mode=Timer.PERIODIC, callback=tock)
+
+while True:
+  print("tick")
+  if tick is True:
+    m5mqtt.publish(MQTT_TOPIC, json.dumps({
+      'temperature': env3_1.temperature,
+      'humidity': env3_1.humidity,
+      'pressure': env3_1.pressure,
+      'device_id': device_id
+    }))
+    
+    rgb.setColorAll(0xff0000)
+    rgb.setBrightness(255)
+    wait_ms(100)
+    rgb.setBrightness(0)
+    
+    wdt.feed()
+    print("tock")
+    tick = False
+    
+  wait_ms(TICK_RESOLUTION)
